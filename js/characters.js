1b5c33051408d34d95d8c6b5a6f3b93cdde67cac
admin <trotmarin@gmail.com>
2022-08-19 17:17:00 +0900

    update

--- js/characters.js
+++ js/characters.js
@@ -1,3 +1,10 @@
+new Character({
+    // xPos: Math.random() *100,
+    xPos: 0,
+    speed: Math.random() * 0.5 + 0.2
+});
+
+
 function Character(info) {
     this.mainElem = document.createElement('div');
     this.mainElem.classList.add('character');
@@ -33,9 +40,9 @@ function Character(info) {
         + '<div class="character-face character-leg-face face-back"></div>'
       + '</div>';
 
-      document.querySelector('.house').appendChild(this.mainElem);
+      document.querySelector('.stage').appendChild(this.mainElem);
       this.mainElem.style.left = info.xPos + '%';
-      console.log("xpos: "+info.xPos);
+      // this.mainElem.style.left = info.xPos;
       // 스크롤중인지 아닌지 확인
       this.scrollState = false;
       //마지막 스크롤 위치
@@ -47,4 +54,82 @@ function Character(info) {
       this.runningState = false;
       this.rafId;
       // this.init();
+}
+
+Character.prototype = {
+  constructor: Character,
+  init: function() {
+    const self = this;
+    window.addEventListener('scroll', function() {
+      clearTimeout(self.scrollState);
+
+      if(!self.scrollState) {
+        self.mainElem.classList.add('running');
+        console.log('running marked')
+      }
+      self.scrollState = setTimeout(function() {
+        self.scrollState = false;
+        self.mainElem.classList.remove('running');
+      }, 500);
+
+      // 이전 스크로 ㄹ위치와 현재 스크롤 위치를 비교
+      if(self.lastScrollTop > pageYOffset) {
+          //스크롤 올림
+         self.mainElem.setAttribute('data-direction', 'backward');
+      } else {
+        //스크롤 내림
+        self.mainElem.setAttribute('data-direction', 'forward');
+
+      }
+
+      // console.log('lastScrollTop: '+ self.lastScrollTop);
+      // console.log('pageYOffset: '+ pageYOffset)
+      self.lastScrollTop = pageYOffset;
+
+      window.addEventListener('keydown', function(e){
+        if(self.runningState) return;
+        if(e.keyCode == 37) {
+          self.direction = 'left';
+          self.mainElem.setAttribute('data-direction', 'left');
+          self.mainElem.classList.add('running');
+          self.run(self);
+          self.runningState = true;
+          // self.xPos = self.xPos - self.speed;
+          // self.mainElem.style.left = self.xPos + '%';
+
+        } else if (e.keyCode == 39) {
+          self.direction = 'right';
+          self.mainElem.setAttribute('data-direction', 'right');
+          self.mainElem.classList.add('running');
+          self.run(self);
+          self.runningState = true;
+        }
+      });
+
+      window.addEventListener('keyup', function(e) {
+        self.mainElem.classList.remove('running');
+        cancelAnimationFrame(self.rafId);
+        self.runningState = false;
+      });
+    }
+    )},
+    run: function(self) {
+      console.log(self.xPos);
+      // const self = this;
+
+
+      if(self.direction == 'left') {
+        self.xPos -= self.speed;
+      } else if (self.direction == 'right') {
+        self.xPos += self.speed;
+      }
+
+      if(self.xPos < 2) self.xPos =2;
+      if(self.xPos > 88) self.xPos =88;
+      self.mainElem.style.left = self.xPos + '%';
+      self.rafId = requestAnimationFrame(function() {
+        self.run(self);
+      });
+
+    }
 }
--- js/wall3d.js
+++ js/wall3d.js
@@ -13,10 +13,26 @@
 
     window.addEventListener('scroll', function () {
         const scrollPer = pageYOffset / maxScrollValue;
+        console.log('-----------------------')
+        console.log('pageYOffset'+pageYOffset);
+        console.log('maxScrollValue'+maxScrollValue);
+        console.log('scrollPer'+scrollPer);
         const zMove = scrollPer* 200 - 100;
-        // const zMove = scrollPer * 200 - 100;
+
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
-        console.log('zMove?;'+zMove)
+
+        wallAElem = document.querySelector('.wall-front-a');
+        var wallAStyle = window.getComputedStyle(wallAElem);
+        var wallAMatrix = new WebKitCSSMatrix(wallAStyle.transform);
+
+        wallBElem = document.querySelector('.wall-front-b');
+        var wallBStyle = window.getComputedStyle(wallBElem);
+        var wallBMatrix = new WebKitCSSMatrix(wallBStyle.transform);
+
+        console.log('houseElem.translateZ:'+zMove);
+        console.log('wallA.translateZ:'+wallAMatrix.m41);
+        console.log('wallB.translateZ:'+wallBMatrix.m41);
+        console.log('-----------------------')
 
         // progress bar
         // barElem.style.width = scrollPer * 100 + '%';
@@ -30,12 +46,32 @@
 
     window.addEventListener('resize', resizeHandler);
 
+
+
     stageElem.addEventListener('click', function (e) {
-        new Character({
-            xPos: e.clientX / window.innerWidth * 100,
-            speed: Math.random() * 0.5 + 0.2
-        });
+        // new Character({
+        //     xPos: e.clientX / window.innerWidth * 100,
+        //     speed: Math.random() * 0.5 + 0.2
+        // });
+        (function () {
+            charElem = document.querySelector('.character');
+            // stageElem = document.querySelector('.stage');
+
+            styleCharElem = charElem.style.left;
+
+            console.log("from: "+styleCharElem);
+            console.log("e.clinetX: "+e.clientX);
+            console.log("e.offsetX:"+e.offsetX);
+            console.log("stage.innerWidth(): "+$('.stage').innerWidth());
+            console.log("window.innerWidth: "+window.innerWidth);
+            console.log("to: "+e.clientX/window.innerWidth*100+"%");
+            // console.log("to_new:"+e.clinetX/stageElem.innerWidth);
+            // console.log("stage_width:"+stageElem.innerWidth);
+        })();
+
     });
+
+
 
     // selectCharacterElem.addEventListener('click', function (e) {
     //     const value = e.target.getAttribute('data-char');
